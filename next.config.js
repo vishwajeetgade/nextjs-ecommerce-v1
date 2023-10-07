ad51a5a9586c65c7b1e8757c21852c1695833688
vishwajeetgade <vishugade15@gmail.com>
2023-10-08 00:19:53 +0530

    images remotePattterns Added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: "images.unsplash.com" }],
+  },
+
+  //  serverActions are stll in Alpha - 08/10/2023
   experimental: {
     serverActions: true,
   },
